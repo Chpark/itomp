97f6aa0ae1d8b37746379978ed1fae8983a4f22f
Chonhyon Park <chpark@cs.unc.edu>
2014-04-28 05:10:15 -0400

    update gitignore

--- move_itomp/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/chonhyon/hydro_workspace/parallel-optimization-based-motion-planner/git/itomp_planner/move_itomp/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
