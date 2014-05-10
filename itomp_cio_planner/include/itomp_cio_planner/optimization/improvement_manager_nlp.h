2e752d9216a4b20734c0bd5c852fa17be487465a
Chonhyon Park <chpark@cs.unc.edu>
2014-05-10 03:23:51 -0400

    refactoring improvement_manager_nlp

--- itomp_cio_planner/include/itomp_cio_planner/optimization/improvement_manager_nlp.h
+++ itomp_cio_planner/include/itomp_cio_planner/optimization/improvement_manager_nlp.h
@@ -4,11 +4,14 @@
 #include <itomp_cio_planner/optimization/improvement_manager.h>
 #include <itomp_cio_planner/common.h>
 #include <itomp_cio_planner/optimization/evaluation_manager.h>
+#include "dlib/optimization.h"
 
 namespace itomp_cio_planner
 {
 
-class ImprovementManagerNLP : public ImprovementManager
+typedef dlib::matrix<double, 0, 1> column_vector;
+
+class ImprovementManagerNLP: public ImprovementManager
 {
 public:
   ImprovementManagerNLP();
@@ -19,12 +22,31 @@ public:
   virtual void runSingleIteration(int iteration);
 
 protected:
-  void optimize(bool add_noise, int iteration);
+  void setVariableVector();
+  void getVariableVector();
+  void addNoiseToVariables();
+
+  double evaluate(const column_vector& variables);
+  column_vector derivative(const column_vector& variables);
+
+  void optimize(int iteration);
 
   int num_dimensions_;
   int num_contact_dimensions_;
   int num_points_;
   int num_contact_phases_;
+
+  int num_contact_vars_free_;
+  int num_pos_variables_;
+  int num_vel_variables_;
+  int num_variables_;
+
+  Eigen::MatrixXd parameters_;
+  Eigen::MatrixXd vel_parameters_;
+  Eigen::MatrixXd contact_parameters_;
+  Eigen::VectorXd costs_;
+
+  column_vector variables_;
 
 };
 
--- itomp_cio_planner/src/optimization/improvement_manager_nlp.cpp
+++ itomp_cio_planner/src/optimization/improvement_manager_nlp.cpp
@@ -1,7 +1,9 @@
 #include <itomp_cio_planner/optimization/improvement_manager_nlp.h>
 #include <itomp_cio_planner/util/multivariate_gaussian.h>
-#include "dlib/optimization.h"
 #include <omp.h>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <functional>
 
 using namespace Eigen;
 
@@ -33,146 +35,34 @@ bool ImprovementManagerNLP::updatePlanningParameters()
   num_points_ = evaluation_manager_->getGroupTrajectory()->getNumPoints();
   num_contact_phases_ = evaluation_manager_->getGroupTrajectory()->getNumContactPhases();
 
+  num_contact_vars_free_ = num_contact_dimensions_ * num_contact_phases_;
+  num_pos_variables_ = num_dimensions_ * (num_contact_phases_ - 1);
+  num_vel_variables_ = num_dimensions_ * (num_contact_phases_ - 1);
+  num_variables_ = num_contact_vars_free_ + num_pos_variables_ + num_vel_variables_;
+
+  variables_.set_size(num_variables_);
+
+  int num_free_points_ = num_contact_phases_ - 1;
+  parameters_ = Eigen::MatrixXd(num_contact_phases_ - 1, num_dimensions_);
+  vel_parameters_ = Eigen::MatrixXd(num_contact_phases_ - 1, num_dimensions_);
+  contact_parameters_ = Eigen::MatrixXd(num_contact_phases_, num_contact_dimensions_);
+  costs_ = Eigen::VectorXd::Zero(num_points_);
+
   return true;
 }
 
 void ImprovementManagerNLP::runSingleIteration(int iteration)
 {
-  bool add_noise = (iteration != 0);
-  optimize(add_noise, iteration);
+  setVariableVector();
+
+  if (iteration != 0)
+    addNoiseToVariables();
+
+  optimize(iteration);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-
-typedef dlib::matrix<double, 0, 1> column_vector;
-Eigen::MatrixXd parameters_;
-Eigen::MatrixXd vel_parameters_;
-Eigen::MatrixXd contact_parameters_;
-Eigen::VectorXd costs_;
-EvaluationManager* evaluation_manager_;
-
-int num_dimensions_;
-int num_contact_dimensions_;
-int num_free_points_;
-int num_points_;
-
-class evaluation_functor
+void ImprovementManagerNLP::setVariableVector()
 {
-
-public:
-  evaluation_functor(EvaluationManager* evaluation_manager, int num_dimensions, int num_contact_dimensions,
-      int num_free_points, int num_points)
-  {
-    evaluation_manager_ = evaluation_manager;
-    num_dimensions_ = num_dimensions;
-    num_contact_dimensions_ = num_contact_dimensions;
-    num_free_points_ = num_free_points;
-    num_points_ = num_points;
-
-    parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
-    vel_parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
-    contact_parameters_ = Eigen::MatrixXd(num_free_points_ + 1, num_contact_dimensions_);
-    costs_ = Eigen::VectorXd::Zero(num_points_);
-  }
-
-  double operator()(const column_vector& variables) const
-  {
-    int readIndex = 0;
-    // copy to group_trajectory_:
-    for (int d = 0; d < num_contact_dimensions_; ++d)
-    {
-      contact_parameters_(0, d) = abs(variables(readIndex + d, 0));
-    }
-    readIndex += num_contact_dimensions_;
-    for (int i = 0; i < num_free_points_; ++i)
-    {
-      for (int d = 0; d < num_dimensions_; ++d)
-      {
-        parameters_(i, d) = variables(readIndex + d, 0);
-      }
-      readIndex += num_dimensions_;
-      for (int d = 0; d < num_dimensions_; ++d)
-      {
-        vel_parameters_(i, d) = variables(readIndex + d, 0);
-      }
-      readIndex += num_dimensions_;
-      for (int d = 0; d < num_contact_dimensions_; ++d)
-      {
-        contact_parameters_(i + 1, d) = abs(variables(readIndex + d, 0));
-      }
-      readIndex += num_contact_dimensions_;
-    }
-
-    return evaluation_manager_->evaluate(parameters_, vel_parameters_, contact_parameters_, costs_);
-  }
-};
-
-column_vector derivative_;
-
-class derivative_functor
-{
-
-public:
-
-  derivative_functor(EvaluationManager* evaluation_manager, int num_dimensions, int num_contact_dimensions,
-      int num_free_points, int num_points)
-  {
-    evaluation_manager_ = evaluation_manager;
-    num_dimensions_ = num_dimensions;
-    num_contact_dimensions_ = num_contact_dimensions;
-    num_free_points_ = num_free_points;
-    num_points_ = num_points;
-
-    parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
-    vel_parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
-    contact_parameters_ = Eigen::MatrixXd(num_free_points_ + 1, num_contact_dimensions_);
-    costs_ = Eigen::VectorXd::Zero(num_points_);
-  }
-
-  column_vector operator()(const column_vector& variables) const
-  {
-    int readIndex = 0;
-    // copy to group_trajectory_:
-    for (int d = 0; d < num_contact_dimensions_; ++d)
-    {
-      contact_parameters_(0, d) = abs(variables(readIndex + d, 0));
-    }
-    readIndex += num_contact_dimensions_;
-    for (int i = 0; i < num_free_points_; ++i)
-    {
-      for (int d = 0; d < num_dimensions_; ++d)
-      {
-        parameters_(i, d) = variables(readIndex + d, 0);
-      }
-      readIndex += num_dimensions_;
-      for (int d = 0; d < num_dimensions_; ++d)
-      {
-        vel_parameters_(i, d) = variables(readIndex + d, 0);
-      }
-      readIndex += num_dimensions_;
-      for (int d = 0; d < num_contact_dimensions_; ++d)
-      {
-        contact_parameters_(i + 1, d) = abs(variables(readIndex + d, 0));
-      }
-      readIndex += num_contact_dimensions_;
-    }
-
-    evaluation_manager_->evaluate(parameters_, vel_parameters_, contact_parameters_, costs_);
-
-    derivative_.set_size(variables.size());
-    return derivative_;
-  }
-};
-
-void ImprovementManagerNLP::optimize(bool add_noise, int iteration)
-{
-  int num_contact_vars_free = num_contact_dimensions_ * num_contact_phases_;
-  int num_pos_variables = num_dimensions_ * (num_contact_phases_ - 1);
-  int num_vel_variables = num_dimensions_ * (num_contact_phases_ - 1);
-  int num_variables = num_contact_vars_free + num_pos_variables + num_vel_variables;
-
-  column_vector variables(num_variables);
-
   int writeIndex = 0;
   // copy from group_trajectory_:
   const Eigen::MatrixXd& freeTrajectoryBlock = evaluation_manager_->getGroupTrajectory()->getFreePoints();
@@ -181,7 +71,7 @@ void ImprovementManagerNLP::optimize(bool add_noise, int iteration)
   // contact variable for phase 0
   for (int d = 0; d < num_contact_dimensions_; ++d)
   {
-    variables(writeIndex + d, 0) = contactTrajectoryBlock(0, d);
+    variables_(writeIndex + d, 0) = contactTrajectoryBlock(0, d);
   }
   writeIndex += num_contact_dimensions_;
 
@@ -189,42 +79,226 @@ void ImprovementManagerNLP::optimize(bool add_noise, int iteration)
   {
     for (int d = 0; d < num_dimensions_; ++d)
     {
-      variables(writeIndex + d, 0) = freeTrajectoryBlock(i, d);
+      variables_(writeIndex + d, 0) = freeTrajectoryBlock(i, d);
     }
     writeIndex += num_dimensions_;
     for (int d = 0; d < num_dimensions_; ++d)
     {
-      variables(writeIndex + d, 0) = freeVelTrajectoryBlock(i, d);
+      variables_(writeIndex + d, 0) = freeVelTrajectoryBlock(i, d);
     }
     writeIndex += num_dimensions_;
     for (int d = 0; d < num_contact_dimensions_; ++d)
     {
-      variables(writeIndex + d, 0) = contactTrajectoryBlock(i, d);
+      variables_(writeIndex + d, 0) = contactTrajectoryBlock(i, d);
     }
     writeIndex += num_contact_dimensions_;
   }
-
-  if (add_noise)
-  {
-    MultivariateGaussian noise_generator(VectorXd::Zero(num_variables),
-        MatrixXd::Identity(num_variables, num_variables));
-    VectorXd noise = VectorXd::Zero(num_variables);
-    noise_generator.sample(noise);
-    for (int i = 0; i < num_variables; ++i)
-    {
-      variables(i) += 0.01 * noise(i);
-    }
-  }
-  /*
-   dlib::find_min(dlib::bfgs_search_strategy(), dlib::objective_delta_stop_strategy(1e-7).be_verbose(),
-   evaluation_functor(this, num_joints_, num_contacts_, num_contact_phases - 1, num_points_),
-   derivative_functor(this, num_joints_, num_contacts_, num_contact_phases - 1, num_points_), variables, -1);
-   */
-  dlib::find_min_using_approximate_derivatives(dlib::bfgs_search_strategy(),
-      dlib::objective_delta_stop_strategy(1e-7).be_verbose(),
-      evaluation_functor(evaluation_manager_, num_dimensions_, num_contact_dimensions_, num_contact_phases_ - 1, num_points_), variables, -1);
-
 }
 
+void ImprovementManagerNLP::getVariableVector()
+{
+  int readIndex = 0;
+  // copy to group_trajectory_:
+  for (int d = 0; d < num_contact_dimensions_; ++d)
+  {
+    contact_parameters_(0, d) = abs(variables_(readIndex + d, 0));
+  }
+  readIndex += num_contact_dimensions_;
+  for (int i = 0; i < num_contact_phases_ - 1; ++i)
+  {
+    for (int d = 0; d < num_dimensions_; ++d)
+    {
+      parameters_(i, d) = variables_(readIndex + d, 0);
+    }
+    readIndex += num_dimensions_;
+    for (int d = 0; d < num_dimensions_; ++d)
+    {
+      vel_parameters_(i, d) = variables_(readIndex + d, 0);
+    }
+    readIndex += num_dimensions_;
+    for (int d = 0; d < num_contact_dimensions_; ++d)
+    {
+      contact_parameters_(i + 1, d) = abs(variables_(readIndex + d, 0));
+    }
+    readIndex += num_contact_dimensions_;
+  }
+}
+
+void ImprovementManagerNLP::addNoiseToVariables()
+{
+  MultivariateGaussian noise_generator(VectorXd::Zero(num_variables_),
+      MatrixXd::Identity(num_variables_, num_variables_));
+  VectorXd noise = VectorXd::Zero(num_variables_);
+  noise_generator.sample(noise);
+  for (int i = 0; i < num_variables_; ++i)
+  {
+    variables_(i) += 0.01 * noise(i);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+ EvaluationManager* evaluation_manager_;
+
+ int num_dimensions_;
+ int num_contact_dimensions_;
+ int num_free_points_;
+ int num_points_;
+ */
+/*
+ class evaluation_functor
+ {
+
+ public:
+ evaluation_functor(EvaluationManager* evaluation_manager, int num_dimensions, int num_contact_dimensions,
+ int num_free_points, int num_points)
+ {
+ evaluation_manager_ = evaluation_manager;
+ num_dimensions_ = num_dimensions;
+ num_contact_dimensions_ = num_contact_dimensions;
+ num_free_points_ = num_free_points;
+ num_points_ = num_points;
+
+ parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
+ vel_parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
+ contact_parameters_ = Eigen::MatrixXd(num_free_points_ + 1, num_contact_dimensions_);
+ costs_ = Eigen::VectorXd::Zero(num_points_);
+ }
+
+ double operator()(const column_vector& variables) const
+ {
+ int readIndex = 0;
+ // copy to group_trajectory_:
+ for (int d = 0; d < num_contact_dimensions_; ++d)
+ {
+ contact_parameters_(0, d) = abs(variables(readIndex + d, 0));
+ }
+ readIndex += num_contact_dimensions_;
+ for (int i = 0; i < num_free_points_; ++i)
+ {
+ for (int d = 0; d < num_dimensions_; ++d)
+ {
+ parameters_(i, d) = variables(readIndex + d, 0);
+ }
+ readIndex += num_dimensions_;
+ for (int d = 0; d < num_dimensions_; ++d)
+ {
+ vel_parameters_(i, d) = variables(readIndex + d, 0);
+ }
+ readIndex += num_dimensions_;
+ for (int d = 0; d < num_contact_dimensions_; ++d)
+ {
+ contact_parameters_(i + 1, d) = abs(variables(readIndex + d, 0));
+ }
+ readIndex += num_contact_dimensions_;
+ }
+
+ return evaluation_manager_->evaluate(parameters_, vel_parameters_, contact_parameters_, costs_);
+ }
+ };
+ */
+/*
+ column_vector derivative_;
+
+ class derivative_functor
+ {
+
+ public:
+
+ derivative_functor(EvaluationManager* evaluation_manager, int num_dimensions, int num_contact_dimensions,
+ int num_free_points, int num_points)
+ {
+ evaluation_manager_ = evaluation_manager;
+ num_dimensions_ = num_dimensions;
+ num_contact_dimensions_ = num_contact_dimensions;
+ num_free_points_ = num_free_points;
+ num_points_ = num_points;
+
+ parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
+ vel_parameters_ = Eigen::MatrixXd(num_free_points_, num_dimensions_);
+ contact_parameters_ = Eigen::MatrixXd(num_free_points_ + 1, num_contact_dimensions_);
+ costs_ = Eigen::VectorXd::Zero(num_points_);
+ }
+
+ column_vector operator()(const column_vector& variables) const
+ {
+ int readIndex = 0;
+ // copy to group_trajectory_:
+ for (int d = 0; d < num_contact_dimensions_; ++d)
+ {
+ contact_parameters_(0, d) = abs(variables(readIndex + d, 0));
+ }
+ readIndex += num_contact_dimensions_;
+ for (int i = 0; i < num_free_points_; ++i)
+ {
+ for (int d = 0; d < num_dimensions_; ++d)
+ {
+ parameters_(i, d) = variables(readIndex + d, 0);
+ }
+ readIndex += num_dimensions_;
+ for (int d = 0; d < num_dimensions_; ++d)
+ {
+ vel_parameters_(i, d) = variables(readIndex + d, 0);
+ }
+ readIndex += num_dimensions_;
+ for (int d = 0; d < num_contact_dimensions_; ++d)
+ {
+ contact_parameters_(i + 1, d) = abs(variables(readIndex + d, 0));
+ }
+ readIndex += num_contact_dimensions_;
+ }
+
+ evaluation_manager_->evaluate(parameters_, vel_parameters_, contact_parameters_, costs_);
+
+ derivative_.set_size(variables.size());
+ return derivative_;
+ }
+ };
+ */
+double ImprovementManagerNLP::evaluate(const column_vector& variables)
+{
+  getVariableVector();
+  return evaluation_manager_->evaluate(parameters_, vel_parameters_, contact_parameters_, costs_);
+}
+
+column_vector ImprovementManagerNLP::derivative(const column_vector& variables)
+{
+  const double eps = 1E-7;
+
+  column_vector der(variables.size());
+  column_vector e(variables);
+
+  for (long i = 0; i < variables.size(); ++i)
+  {
+    const double old_val = e(i);
+
+    e(i) += eps;
+    const double delta_plus = evaluate(e);
+    e(i) = old_val - eps;
+    const double delta_minus = evaluate(e);
+
+    der(i) = (delta_plus - delta_minus) / (2 * eps);
+
+    // and finally restore the old value of this element
+    e(i) = old_val;
+  }
+
+  return der;
+}
+
+void ImprovementManagerNLP::optimize(int iteration)
+{
+  /*
+  dlib::find_min(dlib::bfgs_search_strategy(), dlib::objective_delta_stop_strategy(1e-7).be_verbose(),
+      boost::bind(&ImprovementManagerNLP::evaluate, this, _1),
+      boost::bind(&ImprovementManagerNLP::derivative, this, _1), variables_, -1);
+      */
+
+  dlib::find_min_using_approximate_derivatives(dlib::bfgs_search_strategy(),
+      dlib::objective_delta_stop_strategy(1e-7).be_verbose(), boost::bind(&ImprovementManagerNLP::evaluate, this, _1),
+      variables_, -1);
+
+}
 
 }
