5458ef96a7752aff7d88e98f510ce128caf26e48
Chonhyon Park <chpark@cs.unc.edu>
2014-07-15 00:20:43 -0400

    kuka_description

---
+++ kuka_description/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/chonhyon/hydro_workspace/itomp/kuka_description/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
