4b659c57e178ed1e47d2014202f961deecfe459e
Chonhyon Park <chpark@cs.unc.edu>
2015-05-10 15:14:26 -0400

    get quaternion/exponential map of close side

--- itomp_cio_planner/src/util/exponential_map.cpp
+++ itomp_cio_planner/src/util/exponential_map.cpp
@@ -6,19 +6,32 @@ namespace itomp_cio_planner
 namespace exponential_map
 {
 
-Eigen::Vector3d RotationToExponentialMap(const Eigen::Matrix3d& matrix)
+Eigen::Vector3d RotationToExponentialMap(const Eigen::Matrix3d& matrix, const Eigen::Vector3d* close_to)
 {
-	return QuaternionToExponentialMap(Eigen::Quaterniond(matrix));
+    if (close_to == NULL)
+        return QuaternionToExponentialMap(Eigen::Quaterniond(matrix));
+    else
+    {
+        Eigen::Quaterniond q0 = Eigen::Quaterniond(matrix);
+        Eigen::Vector3d exp_map0 = QuaternionToExponentialMap(q0);
+
+        Eigen::Quaterniond q1 = q0;
+        q1.coeffs() = -q0.coeffs();
+        Eigen::Vector3d exp_map1 = QuaternionToExponentialMap(q1);
+
+        if ((exp_map0 - *close_to).norm() <= (exp_map1 - *close_to).norm())
+            return exp_map0;
+        else
+            return exp_map1;
+    }
 }
 
-Eigen::Matrix3d ExponentialMapToRotation(
-	const Eigen::Vector3d& exponential_rotation)
+Eigen::Matrix3d ExponentialMapToRotation(const Eigen::Vector3d& exponential_rotation)
 {
 	return ExponentialMapToQuaternion(exponential_rotation).toRotationMatrix();
 }
 
-Eigen::Vector3d QuaternionToExponentialMap(
-	const Eigen::Quaterniond& quaternion)
+Eigen::Vector3d QuaternionToExponentialMap(const Eigen::Quaterniond& quaternion)
 {
 	Eigen::Vector3d vec = quaternion.vec();
     if (vec.norm() < ITOMP_EPS)
@@ -29,8 +42,7 @@ Eigen::Vector3d QuaternionToExponentialMap(
 	return theta * vec;
 }
 
-Eigen::Quaterniond ExponentialMapToQuaternion(
-	const Eigen::Vector3d& exponential_rotation)
+Eigen::Quaterniond ExponentialMapToQuaternion(const Eigen::Vector3d& exponential_rotation)
 {
 	double angle = 0.5 * exponential_rotation.norm();
 	Eigen::Quaterniond quaternion;
