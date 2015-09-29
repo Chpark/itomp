e02a01783e2791affe26911f3bb6a13b6ee119a6
Chonhyon Park <chpark@cs.unc.edu>
2015-09-29 12:29:27 -0400

    move params to beta_description

--- itomp_cio_planner/config/upload_param_climb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rosparam load `rospack find itomp_cio_planner`/config/params_climb.yaml itomp_planner
--- itomp_cio_planner/config/upload_param_dodge.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rosparam load `rospack find itomp_cio_planner`/config/params_dodge.yaml itomp_planner
