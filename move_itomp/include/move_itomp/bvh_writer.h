85450f013b66dff2009f24325de9530fc4c1b497
jaesungp <jaesungp@cs.unc.edu>
2015-10-23 21:13:34 -0400

    bvh writing

--- move_itomp/include/move_itomp/bvh_writer.h
+++ move_itomp/include/move_itomp/bvh_writer.h
@@ -2,11 +2,12 @@
 #define BVH_WRITER_H_
 
 #include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit/robot_model/robot_model.h>
 
 namespace bvh_writer
 {
 
-void writeRocketboxTrajectoryBVHFile(moveit_msgs::DisplayTrajectory& display_trajectory, const std::string& filename);
+void writeWalkingTrajectoryBVHFile(const robot_model::RobotModelPtr robot_model, const moveit_msgs::DisplayTrajectory& display_trajectory, const std::string& filename);
 
 }
 
